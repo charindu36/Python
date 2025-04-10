9300f1fe3728bebb1c3ce84c6381655c6eebb7fa
charindu36 <charindumadhusanka801@gmai.com>
2025-04-10 18:56:59 +0530

    new file added

---
+++ right-angled.py
@@ -0,0 +1,13 @@
+# first method
+
+for i in range(6):
+    print("* "*i)
+
+#second method 
+
+n=6
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()    
+  
---
+++ square.py
@@ -0,0 +1,13 @@
+# first method
+
+n=6
+for i in range(n):
+    print("* "*n)
+
+#second method
+
+n=5
+for i in range(n):
+    for j in range(n):
+        print("+",end=" ") 
+    print()    
